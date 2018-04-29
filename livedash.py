d5137cbd0f6037b45ec763b5b91793cc54b9829d
Vasily <vasily.komarov1997@gmail.com>
2018-04-29 20:50:10 +0500

    reworked web plots

--- livedash.py
+++ livedash.py
@@ -10,6 +10,45 @@ import dash_core_components as dcc
 import dash_html_components as html
 from random import random
 import plotly
+from textblob import TextBlob
+import nltk
+import pandas as pd
+import re
+import numpy as np
+import pymongo
+from pymongo import MongoClient
+
+
+
+
+
+#------------------------ для графика со средними значениями полярности
+def get_avg_polarity(mongoArray):    
+    avg_pol = {}
+    for i in mongoArray:
+        if i["date"] in avg_pol:
+            avg_pol[i["date"]].append(i["polarity"])
+        else:
+            avg_pol[i["date"]] = []
+            avg_pol[i["date"]].append(i["polarity"])
+
+    for key in avg_pol.keys():
+        avg_pol[key] = np.mean(avg_pol[key])
+    print ("db working")
+    return avg_pol
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['productiondb']
+db_request = db.main.find()
+
+str_len = str(len(list(db_request)))
+print ("str_len - " + str_len)
+out = get_avg_polarity(db_request)
+
+
+
+
+
 
 app = dash.Dash(__name__)
 colors = {
@@ -25,13 +64,13 @@ app.layout = html.Div(
         }),
 
     html.Div(children='''
-        Here is some information about plot .
-    ''',
+        всего твитов - ''' + str_len 
+    ,
     style={
             'textAlign': 'center',
             'color': colors['text']
         }),
-        dcc.Graph(id='live-update-graph-scatter', animate=True),
+        #dcc.Graph(id='live-update-graph-scatter', animate=True),
         dcc.Graph(id='live-update-graph-bar'),
         dcc.Interval(
             id='interval-component',
@@ -41,33 +80,59 @@ app.layout = html.Div(
 )
 
 
-@app.callback(Output('live-update-graph-scatter', 'figure'),
-              events=[Event('interval-component', 'interval')])
-def update_graph_scatter():
+    
+    
 
-    traces = list()
-    for t in range(2):
-        traces.append(plotly.graph_objs.Scatter(
-            x=[1, 2, 3, 4, 5],
-            y=[(t + 1) * random() for i in range(5)],
-            name='Scatter {}'.format(t),
-            mode= 'lines+markers'
-            ))
-    return {'data': traces}
+# @app.callback(Output('live-update-graph-scatter', 'figure'),
+#               events=[Event('interval-component', 'interval')])
+# def update_graph_scatter():
+#     client = MongoClient('mongodb://localhost:27017/')
+#     db = client['productiondb']
+#     db_request = db.main.find()
+#     out = get_avg_polarity(db_request)
+#     traces = list()
+#     print ("out value - " + str(len(list (out.keys()) ) ))
+#     traces.append(plotly.graph_objs.Scatter(
+#         x=list(out.keys()),
+#         y=list(out.values()),
+#         name='#FIFA 2018',
+#         mode= 'markers'
+#         ))
+#     return {'data': traces}
 
 @app.callback(Output('live-update-graph-bar', 'figure'),
               events=[Event('interval-component', 'interval')])
 def update_graph_bar():
-
+    
+    
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['productiondb']
+    db_request = db.main.find()
+    out = get_avg_polarity(db_request)
+    
     traces = list()
-    for t in range(2):
-        traces.append(plotly.graph_objs.Bar(
-            x=[1, 2, 3, 4, 5],
-            y=[(t + 1) * random() for i in range(5)],
-            name='Bar {}'.format(t)
-            ))
+    
+    traces.append(plotly.graph_objs.Bar(
+        x=list(out.keys()),
+        y=list(out.values()),
+
+        name='#FIFA 2018'
+        ))
     layout = plotly.graph_objs.Layout(
-    barmode='group'
+        barmode='group',
+        xaxis=dict(
+            title='Date',
+            titlefont=dict(
+                family='Arial, sans-serif',
+                size=22,
+                color='black'
+            ),
+            exponentformat='e',
+            showexponent='All',            
+            showticklabels=True
+        )
+        
+    
 )
     return {'data': traces, 'layout': layout}
 
