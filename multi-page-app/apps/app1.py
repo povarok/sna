7104f7c2340dfa8c8b971f2af0bb5105b86ce86d
Иван Ситников <sit7602@gmail.com>
2018-05-07 22:53:57 +0500

    Added pie plot calculation

--- multi-page-app/apps/app1.py
+++ multi-page-app/apps/app1.py
@@ -35,19 +35,33 @@ def get_avg_polarity(mongoArray):
         else:
             avg_pol[i["date"]] = []
             avg_pol[i["date"]].append(i["polarity"])
-
+    
+    #Calc average polarity for each day
     for key in avg_pol.keys():
         avg_pol[key] = np.mean(avg_pol[key])
-    print ("db working")
     return avg_pol
 
-client = pymongo.MongoClient("mongodb://povarok:EDCFVgb1@cluster0-shard-00-00-watg3.mongodb.net:27017,cluster0-shard-00-01-watg3.mongodb.net:27017,cluster0-shard-00-02-watg3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
-db = client['productiondb']
-db_request = db.main.find()
+def get_languages_ratio(mongoArray):
+    lang_ratio = {}
 
-str_len = str(len(list(db_request)))
-print ("str_len - " + str_len)
-out = get_avg_polarity(db_request)
+    for i in mongoArray:
+        if i["language"] in lang_ratio:
+            lang_ratio[i["language"]] += 1
+        else:
+            lang_ratio[i["language"]] = 1
+    return lang_ratio
+        
+
+def get_db():
+    
+    client = pymongo.MongoClient("mongodb://povarok:EDCFVgb1@cluster0-shard-00-00-watg3.mongodb.net:27017,cluster0-shard-00-01-watg3.mongodb.net:27017,cluster0-shard-00-02-watg3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+    db = client['productiondb']
+    db_request = db.fifa_russia.find()
+    #str_len = str(len(list(db_request)))
+    #print ("str_len не из update- " + str_len)
+    #out = get_avg_polarity(db_request)
+
+    return db_request
 
 
 
@@ -56,14 +70,14 @@ colors = {'background': '#7FDBFF','text': '#7FDBFF'}
 layout = html.Div([
     html.Div([
         html.Div([
-            html.H1(children='Dashboard title'),
+            html.H1(children='#fifa #russia'),
         ], className='center-wrap-content'),
     ],className='rs-header'),
 
     html.Div([
         html.Div([
         html.H1(children='Plots'),
-        html.A('Polarity', href='/apps/app1', className='btn-link'),
+        html.A('Twitter', href='/apps/app1', className='btn-link'),
         html.A('Second Plot', href='/apps/app2', className='btn-link'),
         ], className='center-wrap-content'),
     ], className='rs-sidebar-header'),
@@ -79,21 +93,36 @@ layout = html.Div([
         }),
 
     html.Div(children='''
-        Number of tweets - ''' + str_len 
-    ,
-    
-    style={
+        Number of tweets - ''' + str(len(list(get_db()))),
+        id="num_tweets",
+
+        style={
             'textAlign': 'center',
             'color': colors['text']
         }),
+
         #dcc.Graph(id='live-update-graph-scatter', animate=True),
         dcc.Graph(id='live-update-graph-bar'),
         dcc.Interval(
             id='interval-component',
-            interval=1*5000
+            interval=1*7123
         ),
 
-        html.H1(children='Pie Chart example'),
+        dcc.Interval(
+            id='interval-counter',
+            interval=1*10130
+        ),
+
+        dcc.Interval(
+            id='interval-pie',
+            interval=1*4551
+        ),
+
+        html.H1(children='Language percent',
+            style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }),
         dcc.Graph(id='pie-bar'),
         
         html.Div([
@@ -114,13 +143,18 @@ layout = html.Div([
 def update_graph_bar():
     
     
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['productiondb']
-    db_request = db.main.find()
-    out = get_avg_polarity(db_request)
+    # client = pymongo.MongoClient("mongodb://povarok:EDCFVgb1@cluster0-shard-00-00-watg3.mongodb.net:27017,cluster0-shard-00-01-watg3.mongodb.net:27017,cluster0-shard-00-02-watg3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+    
+    
+    # db = client['productiondb']
+    
+    # db_request = db.fifa_russia.find()
+    # print (len(list(db_request)))
+    out = get_avg_polarity(get_db())
     
     traces = list()
-    
+    #str_len = len(list(db_request))
+    #print("из update"+ str_len)
     traces.append(plotly.graph_objs.Bar(
         x=list(out.keys()),
         y=list(out.values()),
@@ -140,24 +174,35 @@ def update_graph_bar():
             showexponent='All',            
             showticklabels=True
         )
-        
-    
-)
+    )
     return {'data': traces, 'layout': layout}
 
 
 
 @app.callback(Output('pie-bar', 'figure'),
-              events=[Event('interval-component', 'interval')])
+              events=[Event('interval-pie', 'interval')])
 def update_pie_bar():
-    labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
-    values = [4500,2500,1053,500]
-
-    trace = plotly.graph_objs.Pie(labels=labels, values=values)
+    out = get_languages_ratio(get_db())
+    labels = list(out.keys())
+    values = list(out.values())
+    print(labels)
+    print(values)
+    trace = plotly.graph_objs.Pie(labels=labels, values=values, textinfo='none', hoverinfo='label+percent')
     
     layout = plotly.graph_objs.Layout(barmode='group')
     
     return {'data': [trace], 'layout': layout}
+
+
+
+@app.callback(
+    Output(component_id='num_tweets', component_property='children'),
+     events=[Event('interval-counter', 'interval')])
+def update_output_div():
+    print("title upd")
+    string = "Number of tweets - " + str(len(list(get_db())))
+    #print (string)
+    return string
 
 
 
