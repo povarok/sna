7b451dfb32ee66987acbfca5ddaaa5a1e25b11e3
Ivan Sitnikov <sit7602@gmail.com>
2018-06-29 11:12:55 +0500

    new mundial csv file

--- worldcup2018/index.py
+++ worldcup2018/index.py
@@ -16,7 +16,7 @@ from watson_developer_cloud.natural_language_understanding_v1 \
 
 #--------------!!!!!!!!!!!!!!!!!!!!-----------------
 #Изменить название файла для анализа!!!!!!!!!!!!
-filename = "mundial.csv"
+filename = "mun.csv"
 #Requirements for Watson
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
